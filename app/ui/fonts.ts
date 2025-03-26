3a935e64b309ed43379848ef0e420a3f0a48ec3a
naimdeb <naimdebard02@gmail.com>
2025-03-26 16:25:03 +0100

    found worse

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
-import { Comic_Neue, Bungee_Tint , Lusitana } from 'next/font/google';
+import { Comic_Neue, Barriecito , Lusitana } from 'next/font/google';
  
 export const comic = Comic_Neue({ subsets: ['latin'], weight: '400' });
-export const inter = Bungee_Tint({ subsets: ['latin'], weight: '400' });
+export const inter = Barriecito({ subsets: ['latin'], weight: '400' });
 export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
